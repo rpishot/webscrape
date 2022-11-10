152763e75bf5ee613ce4cd74953cd57b3dff4def
rpishot <117935259+rpishot@users.noreply.github.com>
2022-11-10 11:28:01 -0600

    Update packages.py

--- packages.py
+++ packages.py
@@ -1,7 +1,7 @@
-import requests
+import requests #this package sends a request to our URL and which responds with the HTML content
 from bs4 import BeautifulSoup #this is the package that we will use to navigate through our HTML code
-import pandas as pd
+import pandas as pd #this is to structure our data into a dataframe
 import re
 import string
 import numpy as np
-import time
+import time #best practice to pause between each request so the site doesn't flag us as a bot
